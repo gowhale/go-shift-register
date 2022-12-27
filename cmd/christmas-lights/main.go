aea5cee8161c856799560e0991f82f2d11d9db47
gowhale <gabewhale@gmail.com>
2022-12-27 16:37:33 +0000

    refactor(comments): remove old comments

--- cmd/christmas-lights/main.go
+++ cmd/christmas-lights/main.go
@@ -21,20 +21,16 @@ func main() {
 	if err := tr.Open(); err != nil {
 		log.Fatalln(err)
 	}
-	// sr := shift.NewRegister(tr, 16, 22, 27, 8)
-	// sr2 := shift.NewRegister(tr, 21, 20, 12, 8)
-	sr3 := shift.NewRegisterHardware(tr, 5, 6, 13, 32)
+	sr := shift.NewRegisterHardware(tr, 5, 6, 13, 32)
 
 	defer func() {
-		// sr.Clear()
-		// sr2.Clear()
-		sr3.Clear()
+		sr.Clear()
 		if err := tr.Close(); err != nil {
 			log.Fatalln(err)
 		}
 	}()
 
-	if err := merryChristmas(sr3); err != nil {
+	if err := merryChristmas(sr); err != nil {
 		log.Fatalln(err)
 	}
 }
@@ -53,7 +49,6 @@ func everyOtherLight(bits, offset int) []int {
 
 func merryChristmas(sr shift.Register) error {
 	for {
-		// for cycle := 0; cycle < 100; cycle++ {
 		for count := 0; count < sr.NOutputs(); count++ {
 			combo := lightAddition(sr.NOutputs(), count)
 			if err := sr.ShowCombo(combo); err != nil {
@@ -62,7 +57,6 @@ func merryChristmas(sr shift.Register) error {
 			time.Sleep(time.Millisecond * 300)
 		}
 
-		// for count := 0; count < 100; count++ {
 		combo := everyOtherLight(sr.NOutputs(), 0)
 		if err := sr.ShowCombo(combo); err != nil {
 			return err
